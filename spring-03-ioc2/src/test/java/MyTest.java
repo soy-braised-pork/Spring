27965ff6251022268cc027a62d0a846588ab9c03
soy-braised-pork <2428784410@qq.com>
2021-05-22 07:02:51 +0800

    update

--- spring-03-ioc2/src/test/java/MyTest.java
+++ spring-03-ioc2/src/test/java/MyTest.java
@@ -7,14 +7,17 @@ public class MyTest {
 
     public static void main(String[] args) {
 
-
-        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        //在配置文件初始化时，对象就已经创建了
+//        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml","applicationContext.xml");
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
         User user = (User) context.getBean("user");
         User user1 = (User) context.getBean("user");
+        User user2= (User) context.getBean("abc");
         user.show();
         //spring容器，取的是同一个对象  对象池
         System.out.println(user==user1);
         System.out.println(user.equals(user1));
+        System.out.println(user2.getName());  //别名
 
         UserT userT=new UserT();
         System.out.println(userT.getName());
