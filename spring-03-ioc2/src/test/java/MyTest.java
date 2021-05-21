fda10c3589c80943aa46961dbd97707ce73d48ed
soy-braised-pork <2428784410@qq.com>
2021-05-21 09:20:35 +0800

    update

--- spring-03-ioc2/src/test/java/MyTest.java
+++ spring-03-ioc2/src/test/java/MyTest.java
@@ -5,9 +5,9 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class MyTest {
 
     public static void main(String[] args) {
-//        User user=new User();
-//        System.out.println(user.getName());
-//        System.out.println(user);
+        User user=new User();
+        System.out.println(user.getName());
+        System.out.println(user);
 
         ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
         User user = (User) context.getBean("user");
