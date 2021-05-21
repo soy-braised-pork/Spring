c4deb7ac83f75a6666dd015cbf646f3a319293b8
soy-braised-pork <2428784410@qq.com>
2021-05-22 07:52:01 +0800

    update

---
+++ spring-04-di/src/main/java/com/zlx/easy/Address.java
@@ -0,0 +1,14 @@
+package com.zlx.easy;
+
+public class Address {
+    private String address;
+
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
