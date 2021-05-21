82d379fd46a21878a65c30d8b28e9a9fb5c0f42e
soy-braised-pork <2428784410@qq.com>
2021-05-21 21:53:44 +0800

    update

--- spring-03-ioc2/src/test/java/MyTest.java
+++ spring-03-ioc2/src/test/java/MyTest.java
@@ -1,16 +1,24 @@
 import com.zlx.easy.User;
+import com.zlx.easy.UserT;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MyTest {
 
     public static void main(String[] args) {
-        User user=new User();
-        System.out.println(user.getName());
-        System.out.println(user);
+
 
         ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
         User user = (User) context.getBean("user");
+        User user1 = (User) context.getBean("user");
         user.show();
+        //spring容器，取的是同一个对象  对象池
+        System.out.println(user==user1);
+        System.out.println(user.equals(user1));
+
+        UserT userT=new UserT();
+        System.out.println(userT.getName());
+        System.out.println(userT);
+
     }
 }
