eb045beea65e28a020afdf7b6c3b09730228e338
soy-braised-pork <2428784410@qq.com>
2021-05-26 12:43:00 +0800

    update

--- database/src/main/java/cpool/ConnectionPoolImpl.java
+++ database/src/main/java/cpool/ConnectionPoolImpl.java
@@ -1,68 +1,68 @@
-package cpool;
-
-import java.sql.Connection;
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.atomic.AtomicInteger;
-
-public class ConnectionPoolImpl implements ConnectionPool {
-
-    /**
-     * 空闲连接池
-     */
-    private LinkedBlockingQueue<Connection> idleConnectPool;
-
-    /**
-     * 活跃连接池
-     */
-    private LinkedBlockingQueue<Connection> busyConnectPool;
-
-    /**
-     * 当前正在被使用的连接数
-     */
-    private AtomicInteger activeSize = new AtomicInteger(0);
-
-    /**
-     * 最大连接数
-     */
-    private final int maxSize;
-
-    public ConnectionPoolImpl(int maxSize) {
-        this.maxSize = maxSize;
-        init();  //init pool
-    }
-
-
-    @Override
-    public void init() {
-        idleConnectPool = new LinkedBlockingQueue<>();
-        busyConnectPool = new LinkedBlockingQueue<>();
-
-    }
-
-    @Override
-    public Connection getConnection() {
-        //从idle池中取出一个连接
-        Connection connection=idleConnectPool.poll();
-        if (connection!=null){
-            //如果有连接，则放入busy池中
-            busyConnectPool.offer(connection);
-            System.out.println("获取到连接");
-            return connection;
-        }
-        return null;
-    }
-
-
-    /**
-     *释放一个连接
-     */
-    @Override
-    public void releaswConnection(Connection connection) {
-
-    }
-
-    @Override
-    public void destroy() {
-
-    }
-}
+//package cpool;
+//
+//import java.sql.Connection;
+//import java.util.concurrent.LinkedBlockingQueue;
+//import java.util.concurrent.atomic.AtomicInteger;
+//
+//public class ConnectionPoolImpl implements ConnectionPool {
+//
+//    /**
+//     * 空闲连接池
+//     */
+//    private LinkedBlockingQueue<Connection> idleConnectPool;
+//
+//    /**
+//     * 活跃连接池
+//     */
+//    private LinkedBlockingQueue<Connection> busyConnectPool;
+//
+//    /**
+//     * 当前正在被使用的连接数
+//     */
+//    private AtomicInteger activeSize = new AtomicInteger(0);
+//
+//    /**
+//     * 最大连接数
+//     */
+//    private final int maxSize;
+//
+//    public ConnectionPoolImpl(int maxSize) {
+//        this.maxSize = maxSize;
+//        init();  //init pool
+//    }
+//
+//
+//    @Override
+//    public void init() {
+//        idleConnectPool = new LinkedBlockingQueue<>();
+//        busyConnectPool = new LinkedBlockingQueue<>();
+//
+//    }
+//
+//    @Override
+//    public Connection getConnection() {
+//        //从idle池中取出一个连接
+//        Connection connection=idleConnectPool.poll();
+//        if (connection!=null){
+//            //如果有连接，则放入busy池中
+//            busyConnectPool.offer(connection);
+//            System.out.println("获取到连接");
+//            return connection;
+//        }
+//        return null;
+//    }
+//
+//
+//    /**
+//     *释放一个连接
+//     */
+//    @Override
+//    public void releaswConnection(Connection connection) {
+//
+//    }
+//
+//    @Override
+//    public void destroy() {
+//
+//    }
+//}
--- spring-05-Autowired/src/main/java/com/zlx/easy/People.java
+++ spring-05-Autowired/src/main/java/com/zlx/easy/People.java
@@ -1,14 +1,28 @@
 package com.zlx.easy;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.lang.Nullable;
+
+import javax.annotation.Resource;
 
 public class People {
 
+    /**
+     * 自动装配
+     * 如果@Autowired自动装配的环境比较复杂，自动装配无法通过@Autowired完成的时候，
+     * 使用@Qualifier(value="xxx")去配置@Autowired的使用，指定一个唯一的bean对象注入
+     */
     //在属性、set方式上使用
-    @Autowired  //不需要get set
+    //不需要get set
+    //如果显示定义了Autowired的required属性为false，说明这个对象可以为null，否则不允许为空
+    @Autowired(required = false)
+    @Qualifier(value = "cat11")
     private Cat cat;
 
+    //java
+    //先通过名字去查找，名字找不到再适配类型，最后不行就报错
+    @Resource
     private Dog dog;
 
     private String name;
