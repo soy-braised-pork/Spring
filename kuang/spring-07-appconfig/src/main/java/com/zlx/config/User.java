10040e89974e6d4b8eaea71df3175474763de759
soy-braised-pork <2428784410@qq.com>
2021-06-04 15:51:31 +0800

    update

--- kuang/spring-07-appconfig/src/main/java/com/zlx/config/User.java
+++ kuang/spring-07-appconfig/src/main/java/com/zlx/config/User.java
@@ -1,4 +1,20 @@
 package com.zlx.config;
 
 public class User {
+    private int age;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "age=" + age +
+                '}';
+    }
 }
