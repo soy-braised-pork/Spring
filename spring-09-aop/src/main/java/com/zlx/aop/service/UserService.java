0a11f99ea4b89bacc92bc6dedf898e87f0a7f7df
soy-braised-pork <2428784410@qq.com>
2021-06-07 14:08:59 +0800

    update

---
+++ spring-09-aop/src/main/java/com/zlx/aop/service/UserService.java
@@ -0,0 +1,9 @@
+package com.zlx.aop.service;
+
+public interface UserService {
+
+    public void add();
+    public void delete();
+    public void update();
+
+}
---
+++ spring-09-aop/src/main/java/com/zlx/aop/service/impl/UserServiceImpl.java
@@ -0,0 +1,20 @@
+package com.zlx.aop.service.impl;
+
+import com.zlx.aop.service.UserService;
+
+public class UserServiceImpl implements UserService {
+    @Override
+    public void add() {
+
+    }
+
+    @Override
+    public void delete() {
+
+    }
+
+    @Override
+    public void update() {
+
+    }
+}
